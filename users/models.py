d978f8960bc98275ada9f642801e9259ed6b5ab0
nahua3730 <nahua3730@gmail.com>
2026-02-12 15:53:41 -0500

    Add recruiter title field and show it on recruiter profile

--- users/models.py
+++ users/models.py
@@ -43,6 +43,10 @@ class RecruiterProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='recruiter_profile')
     company_name = models.CharField(max_length=255)
-    
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="e.g. Technical Recruiter, Hiring Manager"
+    )
     def __str__(self):
-        return self.company_name
+        return f"{self.user.get_full_name()} - {self.company_name}"
